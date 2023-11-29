bbb3608b82b9ffafe79473724f4c7d42d7da858d
alejandr000091 <alejandr000091@gmail.com>
2023-11-30 00:19:29 +0200

    test_pipenv

---
+++ setup.py
@@ -0,0 +1,8 @@
+#work1
+from setuptools import setup, find_namespace_packages
+
+setup(name = "bh",
+      versi",
+      license='MIT',
+      packages=find_namespace_packages(),
+      entry_points={'console_scripts': ['bh = hw_1_01.hw_1_01:main']})
