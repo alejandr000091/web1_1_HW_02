1e0d4cb4ac05214e85bb9dc8137bff4ae6c093ab
alejandr000091 <alejandr000091@gmail.com>
2023-12-01 20:37:34 +0200

    commit with gitignore

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #work1
 from setuptools import setup, find_namespace_packages
 
-setup(name = "bh",
-      versi",
+setup(name = "bhb",
+      version="0.11.4",
       license='MIT',
       packages=find_namespace_packages(),
-      entry_points={'console_scripts': ['bh = hw_1_01.hw_1_01:main']})
+      entry_points={'console_scripts': ['bhb = hw_1_01.hw_1_01:main']})
